0f85f3629bb3f83410b11fd931bd9976e8404617
animevn <haanhth21@gmail.com>
2019-12-21 17:40:24 +0700

    add activity

--- app/src/main/java/com/haanhgs/app/intentdemo/SecondActivity.java
+++ app/src/main/java/com/haanhgs/app/intentdemo/SecondActivity.java
@@ -1,13 +1,15 @@
 package com.haanhgs.app.intentdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
     }
+
 }
