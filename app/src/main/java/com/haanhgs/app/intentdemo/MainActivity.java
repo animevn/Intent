3d4075a1b51392af1ec29690c413d5e4f7a803dd
animevn <haanhth21@gmail.com>
2019-12-21 17:45:26 +0700

    main ok

--- app/src/main/java/com/haanhgs/app/intentdemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/intentdemo/MainActivity.java
@@ -1,13 +1,49 @@
 package com.haanhgs.app.intentdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvMain)
+    TextView tvMain;
+    @BindView(R.id.etMessage)
+    EditText etMessage;
+    @BindView(R.id.bnSend)
+    Button bnSend;
+
+    public static final int SEND_REQUEST = 555;
+    public static final String SEND = "send";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    private void sendMessage(){
+
+        if (!TextUtils.isEmpty(etMessage.getText())){
+            Intent intent = new Intent(this, SecondActivity.class);
+            intent.putExtra(SEND, etMessage.getText().toString());
+            startActivityForResult(intent, SEND_REQUEST);
+
+            tvMain.setText("");
+            etMessage.setText("");
+        }
+    }
+
+    @OnClick(R.id.bnSend)
+    public void onViewClicked() {
+        sendMessage();
     }
 }
--- app/src/main/java/com/haanhgs/app/intentdemo/SecondActivity.java
+++ app/src/main/java/com/haanhgs/app/intentdemo/SecondActivity.java
@@ -1,15 +1,32 @@
 package com.haanhgs.app.intentdemo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SecondActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvSecond)
+    TextView tvSecond;
+    @BindView(R.id.etReply)
+    EditText etReply;
+    @BindView(R.id.bnReply)
+    Button bnReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButterKnife.bind(this);
 
     }
 
+    @OnClick(R.id.bnReply)
+    public void onViewClicked() {
+    }
 }
